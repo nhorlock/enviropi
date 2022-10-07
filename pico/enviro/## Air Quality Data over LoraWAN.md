7c1c9c28bc9baab6c315418482ab51f7b6fbe29f
Neil <nhorlock@gmail.com>
2022-10-07 23:00:50 +0100

    more wip updates. add readme.md with braindump for lora payload
    remove legacy file

---
+++ pico/enviro/## Air Quality Data over LoraWAN.md
@@ -0,0 +1,32 @@
+## Air Quality Data over LoraWAN
+### Problem
+LoRa is the ideal distribution network for remotely deployed Air Qality sensors being long-range, lower power and in many cases not needed a subscription. However, the bandwidth is very limited. 
+
+For our air quality monitoring needs we have a minimal data set that comprises
+PM1, PM2.5 and PM10 particulate matter readings.
+
+| Field name | size |bytes| description |
+|:-------|:----|----:|:-------|
+|Time|64 bit integer|8|Time of reading (this may be replaced by the LoRaWAN packet time?)
+|Temp|double|8|Ambient Temperature
+|DeviceTemp|double|8|Device Temperature
+|Gas|64-bit integer|8|Gas sensor resistance
+|PM1|64-bit integer|8|1 um particles
+|PM2.5|64-bit integer|8|2.5 um particles
+|PM10|64-bit integer|8|10 um particles
+|Humidity|double|8|Humidity
+|Pressure|double|8|Pressure
+|Lux|double|8|Light levels
+|Total||80
+
+[The Things Network Limitations page](https://www.thethingsnetwork.org/docs/lorawan/limitations/) suggests the following.
+```
+Payload should be as small as possible. This means that you should not send JSON or plain (ASCII) text, but instead encode your data as binary data. This is made really easy with the Cayenne Low Power Payload format which is fully supported by The Things Network.
+
+Interval between messages should be in the range of several minutes, so be smart with your data. You could for example transmit a min|avg|max every 5 minutes, or you could only transmit when you sensor value changed more than a certain threshold or have it triggered by motion or another event.
+
+Data Rate should be as fast as possible to minimize your airtime. SF7BW125 is usually a good place to start, as it consumes the least power and airtime. If you need more range, you can slowly increase until you have enough. You can also enable adaptive data rate (ADR), the network will then be able to automatically optimize your data rate.
+```
+
+
+
--- pico/enviro/main.py
+++ pico/enviro/main.py
@@ -1,52 +1,28 @@
-## Pi Pico + Enviro+ pack
-# each loop will read all the sensors and send K=V data off of the device (default is USB)
-# derived form the arduino environ+ example in this repo
-#
+'''
+Particle Sensor Example
 
-import math
-import gc
+This example requires seperate MicroPython drivers for the PMS5003 particulate sensor.
+(You can find it at https://github.com/pimoroni/pms5003-micropython )
+or install from PyPi by searching for 'pms5003-micropython' in Thonny's 'Tools > Manage Packages'
 
-# import board
-# import busio
-from breakout_bme68x import BreakoutBME68X, STATUS_HEATER_STABLE
-from pimoroni_i2c import PimoroniI2C
+'''
 from picographics import PicoGraphics, DISPLAY_ENVIRO_PLUS
 from pimoroni import RGBLED
 from pms5003 import PMS5003
+from pimoroni_i2c import PimoroniI2C
 from breakout_ltr559 import BreakoutLTR559
+from breakout_bme68x import BreakoutBME68X, STATUS_HEATER_STABLE
+
 
 import machine
 import time
 
-# import analogio
-# import displayio
-# import pulseio
-# import terminalio
-# import microcontroller
+print("""particle.py - Continuously print all data values.
+and draw a pretty histogram on display
+""")
 
-# settings
-interval = 5 # seconds delay between readings
-sea_level_pressure = 1013.25
 
-def initialise_i2c():
-    PINS_BREAKOUT_GARDEN = {"sda": 4, "scl": 5}
-    PINS_PICO_EXPLORER = {"sda": 20, "scl": 21}
-
-    i2c = PimoroniI2C(**PINS_BREAKOUT_GARDEN)
-    return i2c
-
-def initialise_bme688(i2c):
-    # Change this to match the location's pressure (hPa) at sea level
-    bme = BreakoutBME68X(i2c, address=0x77)
-    bme.sea_level_pressure = sea_level_pressure
-    bme.mode = adafruit_bme280.MODE_NORMAL
-    bme.standby_period = adafruit_bme280.STANDBY_TC_500
-    bme.iir_filter = adafruit_bme280.IIR_FILTER_X16
-    bme.overscan_pressure = adafruit_bme280.OVERSCAN_X16
-    bme.overscan_humidity = adafruit_bme280.OVERSCAN_X1
-    bme.overscan_temperature = adafruit_bme280.OVERSCAN_X2
-    return bme280
-
+# Configure the PMS5003 for Enviro+
 def initialise_pms5003():
     # Configure the PMS5003 for Enviro+
     pms5003 = PMS5003(
@@ -63,86 +39,128 @@ def initialise_pms5003():
         return None
     return pms5003
 
+def initialise_bme688(i2c):
+    # Change this to match the location's pressure (hPa) at sea level
+    bme = BreakoutBME68X(i2c, address=0x77)
+    # bme.configure(FILTER_COEFF_63, STANDBY_TIME_500_MS, OVERSAMPLING_X16, OVERSAMPLING_X2, OVERSAMPLING_X1)
+# TODO    bme.sea_level_pressure = sea_level_pressure
+# TODO   bme.mode = adafruit_bme280.MODE_NORMAL
+
+    #bme.standby_period = adafruit_bme280.STANDBY_TC_500
+    #bme.iir_filter = adafruit_bme280.IIR_FILTER_X16
+    #bme.overscan_pressure = adafruit_bme280.OVERSCAN_X16
+    #bme.overscan_humidity = adafruit_bme280.OVERSCAN_X1
+    #bme.overscan_temperature = adafruit_bme280.OVERSCAN_X2
+    return bme
+
+def initialise_i2c():
+    PINS_BREAKOUT_GARDEN = {"sda": 4, "scl": 5}
+    PINS_PICO_EXPLORER = {"sda": 20, "scl": 21}
+
+    i2c = PimoroniI2C(**PINS_BREAKOUT_GARDEN)
+    return i2c
+
+display = PicoGraphics(display=DISPLAY_ENVIRO_PLUS)
+display.set_backlight(1.0)
+
+# Setup background
+BG = display.create_pen(0, 0, 0)
+TEXT = display.create_pen(255, 255, 255)
+PM10 = display.create_pen(255, 0, 0)
+PM25 = display.create_pen(255, 255, 0)
+PM100 = display.create_pen(0, 255, 0)
+PM125 = display.create_pen(255, 255, 0)
+PM1000 = display.create_pen(255, 255, 0)
+display.set_pen(BG)
+display.clear()
+
+def initialise_LED():
+    # Setup RGB Led
+    led = RGBLED(6, 7, 10, invert=True)
+    led.set_rgb(0, 0, 0)
+    return led
+
 def initialise_ltr559(i2c):
     # set up connection with the ltr559
     ltr559 = BreakoutLTR559(i2c)
     return ltr559
 
+# Drawing routines
+
+def initialise_display():
+    global display
+    display.set_backlight(1.0)
+    # Setup background
+    display.clear()
+
+def draw_background():
+    display.set_pen(BG)
+    display.clear()
+    display.set_pen(TEXT)
+    display.text("PicoMon v0.2", 5, 10, scale=3)
+
+
+def draw_txt_overlay(sensor_data):
+    display.set_pen(PM10)
+    display.text("PM1.0: {0}".format(sensor_data.pm_ug_per_m3(1.0)), 5, 60, scale=3)
+    display.set_pen(PM25)
+    display.text("PM2.5: {0}".format(sensor_data.pm_ug_per_m3(2.5)), 5, 80, scale=3)
+    display.set_pen(PM100)
+    display.text("PM10: {0}".format(sensor_data.pm_ug_per_m3(10)), 5, 100, scale=3)
+
+
+# settings
+interval = 5 # seconds delay between readings
+sea_level_pressure = 1013.25
+readings = {}
+display = PicoGraphics(display=DISPLAY_ENVIRO_PLUS)
+initialise_display()
+led = initialise_LED()
+
 # Create library object using our Bus I2C port
 i2c = initialise_i2c()
-
-# pressure, humidity, temp
-bme280 = initialise_bme688(i2c)
-# particulates
-pms5003 = initialise_pms5003()
-# gas needs no initialisation
-# proximity and light 
-ltr559 = initialise_ltr559(i2c)
-
 # microphone
 # set up mic input
 MIC_PIN = 26
-mic = ADC(Pin(26))
+# mic = ADC(Pin(26)) B0RK
 
-# The sensor will need a moment to gather initial readings
-time.sleep(1)
+pms5003 = initialise_pms5003()
 
+bme = initialise_bme688(i2c)
+
+# proximity and light 
+ltr559 = initialise_ltr559(i2c)
 # record the time that the sampling starts
-last_reading = time.monotonic()
-last_sec = time.monotonic()
+# last_reading = time.monotonic()
+# last_sec = time.monotonic()
 num_idle_loops=0
 num_loops=0
 cum_noise=0
+
+print("Setup complete..loop starting")
 while True:
-    num_loops+=1
-    # if 1 second has passed
-    # if interval time has passed since last reading
-    if last_reading + interval < time.monotonic():
-        # take readings
-        # take the light reading
-        readings={}
-        readings["lux"] = ltr559.get_lux()
-        # temp etc
-        readings["ucontroller_cpu_temp"] = microcontroller.cpu.temperature
-        readings["temperature"] = bme280.temperature
-        readings["pressure"] = bme280.pressure
-        readings["humidity"] = bme280.humidity
-        #altitude = bme280.altitude # uncomment for altitude estimation
-        # gases
-        gas_reading = gas.read_all()
-        readings["OX"]=gas_reading._OX.value * (gas_reading._OX.reference_voltage/65535)
-        readings["RED"]=gas_reading._RED.value * (gas_reading._RED.reference_voltage/65535)
-        readings["NH3"]=gas_reading._NH3.value * (gas_reading._NH3.reference_voltage/65535)
-        readings["OX_raw"]=gas_reading._OX.value
-        readings["RED_raw"]=gas_reading._RED.value
-        readings["NH3_raw"]=gas_reading._NH3.value
+    num_loops += 1
+    draw_background()
+    if ltr559 is not None:
+        readings["lux"] = ltr559.get_reading()[BreakoutLTR559.LUX]
+    if bme is not None:
+        temperature, pressure, humidity, gas, status, _, _ = bme.read()
+        if status & STATUS_HEATER_STABLE:
+            readings["temperature"] = temperature
+            readings["pressure"] = pressure
+            readings["humidity"] = humidity
+            readings["gas_resistance"] = gas
+    if pms5003 is not None:
+        pms_reading = pms5003.read()
+        readings["pm1"] = pms_reading.data[0]
+        readings["pm2"] = pms_reading.data[1]
+        readings["pm10"] = pms_reading.data[2]
+        readings["pm1_atmos"] = pms_reading.data[0]
+        readings["pm2_atmos"] = pms_reading.data[1]
+        readings["pm10_atmos"] = pms_reading.data[2]
+        # print(pms_reading)
+        print(readings)
 
-        # get the sound readings (number of samples over the threshold / total number of samples taken) and apply a logarithm function to them to represent human hearing
-        readings["sound_level"] = cum_noise/num_idle_loops
-        readings["num_loops"] = num_loops
-        readings["num_idle_loops"] = num_idle_loops
-
-        if pms5003 is not None:
-            # take readings
-            pms_reading = pms5003.read()
-            readings["pm1"] = pms_reading.data[0]
-            readings["pm2"] = pms_reading.data[1]
-            readings["pm10"] = pms_reading.data[2]
-            readings["pm1_atmos"] = pms_reading.data[0]
-            readings["pm2_atmos"] = pms_reading.data[1]
-            readings["pm10_atmos"] = pms_reading.data[2]
-        print("BEGIN")
-        for k in readings.keys():
-            print("{}={}".format(k, readings[k]))
-        print("END")
-        # record the time that this reading was taken
-        last_reading = time.monotonic()
-
-        num_idle_loops=0
-        num_loops=0
-        cum_noise=0
-    else:
-        cum_noise += abs(mic.read_u16()-32768)
-        num_idle_loops+=1
-    # update the last_sec time
-
+    draw_txt_overlay(pms_reading)
+    display.update()
+    time.sleep(0.5)
